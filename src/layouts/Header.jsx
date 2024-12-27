69554c864f65c5d1cdf9e0fb690bb66cfc00d70a
Burak-Salca <salcaburak@gmail.com>
2024-12-28 01:06:36 +0300

    Header bitti

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { useLocation, Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function Header() {
-    
+    const location = useLocation();
+    const isActive = (path) => location.pathname === path;
+
   return (
     <div>
         <header className="relative w-full h-[207px] bg-red flex justify-center items-center">
@@ -10,13 +12,13 @@ export default function Header() {
             src="/images/iteration-1-images/logo.svg"
             alt="Logo"/>
                  <nav className="absolute bottom-4 left-4 flex items-center gap-2 ">
-                    <Link to="/" className='text-white font-barlow hover:font-bold '>
+                    <Link to="/" className={`text-white font-barlow ${isActive('/') ? 'font-bold' : ''} hover:font-bold`}>
                         Ana Sayfa
                     </Link>
                     <span className="text-white text-lg font-bold font-barlow">
                         →
                     </span>
-                    <span className='text-lg font-bold font-barlow text-white'>
+                    <span className={`text-lg font-bold font-barlow ${isActive('/orderPage') ? 'font-bold' : 'text-white'}`}>
                         Sipariş Sayfası
                     </span>
                 </nav>
