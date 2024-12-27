01868434ad00955858a1c4467f21f9d5fc654e38
Burak-Salca <salcaburak@gmail.com>
2024-12-27 19:37:32 +0300

    Home Page bitti

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function HomePage() {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push('/orderPage'); 
+  };
+
   return (
-    <div>HomePage</div>
+    <div
+      className="flex flex-col justify-center items-center h-screen bg-cover bg-center "
+      style={{
+        backgroundImage: "url('/images/iteration-1-images/home-banner.png')",
+    }}>
+      <img className=" h-auto mt-12 mb-4"
+        src="/images/iteration-1-images/logo.svg"
+        alt="Logo"/>
+      <img className=" h-auto mb-4" 
+        src="images\iteration-1-images\Group 4.svg"
+        alt="Logo"/>
+      <button onClick={handleClick} className="w-[193.27px] h-[56px] bg-yellow rounded-3xl text-black font-bold">
+        SİPRAİŞ VER
+      </button>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: '#FDC913',
+        lightGray: '#5F5F5F',
+        darkGray: '#292929',
+        red: '#CE2829',
+        beige: '#FAF7F2',
+      },
+    },
   },
   plugins: [],
 }
